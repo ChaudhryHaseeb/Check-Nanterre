df01f270940dd4359e10132195b152d483e307cd
Damien <37009779@parisnanterre.fr>
2020-02-06 21:45:27 +0100

    add users + create

--- utilisateur/models.py
+++ utilisateur/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Etudiant(models.Model):
+class Utilisateur(models.Model):
     # Lien entre l'etudiant et le modele User de django
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    numero_etudiant = models.IntegerField(blank=True, null=True)
+    ETU = 'Etudiant'
+    PROF = 'Professeur'
+    SERE = 'Secretaire'
+    role_choix = (
+        (ETU, 'Etudiant'),
+        (PROF, 'Professeur'),
+        (SERE, 'Secretaire')
+    )
+    role = models.CharField(max_length=10, choices=role_choix, default=ETU)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
