f9537ea38ba86cb23f7b770bbe006bcf7d1dece4
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2020-02-12 12:02:23 +0100

    Ajout de la matiere

--- absence/urls.py
+++ absence/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('creation/matiere', views.creer_matiere, name='creer_matiere'),
+    path('afficher/listeMatiere', views.liste_matiere, name='liste_matiere'),
 ]
--- absence/views.py
+++ absence/views.py
@@ -1,8 +1,27 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+from absence.forms import CreationMatiere
+from absence.models import Matiere
+from utilisateur.views import verifSecretaire
 
 
-def test(request):
-    return HttpResponse("Test")
+@login_required(login_url="/utilisateur/connexion")
+@user_passes_test(verifSecretaire, login_url="/utilisateur/deconnexion")
+def creer_matiere(request):
+    if request.method == 'POST':
+        form = CreationMatiere(request.POST)
+        if form.is_valid():
+            titre = request.POST.get('titre', '')
+            Matiere.objects.create(titre=titre)
+            print("bien créer")
+    else:
+        form = CreationMatiere()
+    return render(request, 'creer_matiere.html', {'form': form})
+
+
+@login_required(login_url="/utilisateur/connexion")
+@user_passes_test(verifSecretaire, login_url="/utilisateur/deconnexion")
+def liste_matiere(request):
+    matieres = Matiere.objects.all()
+    return render(request, 'liste_matiere.html',{'matieres': matieres})
