0bd93bc79655c442680bfce0a354a99a671ab9a0
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2020-02-14 13:03:40 +0100

    Ajout de la suppresion et de la modification de la matière

--- absence/views.py
+++ absence/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from absence.forms import CreationMatiere
+from absence.forms import CreationMatiere, ModifierMatiere
 from absence.models import Matiere
 from utilisateur.views import verifSecretaire
 
@@ -12,9 +15,13 @@ def creer_matiere(request):
     if request.method == 'POST':
         form = CreationMatiere(request.POST)
         if form.is_valid():
-            titre = request.POST.get('titre', '')
-            Matiere.objects.create(titre=titre)
-            print("bien créer")
+            try:
+                titre = request.POST.get('titre', '')
+                Matiere.objects.create(titre=form.cleaned_data['titre'])
+                messages.success(request, "Le titre de la matière a été crée avec succès !")
+            except:
+                messages.error(request, "Erreur dans le formulaire !")
+            return HttpResponseRedirect(reverse('liste_matiere'))
     else:
         form = CreationMatiere()
     return render(request, 'creer_matiere.html', {'form': form})
@@ -24,4 +31,32 @@ def creer_matiere(request):
 @user_passes_test(verifSecretaire, login_url="/utilisateur/deconnexion")
 def liste_matiere(request):
     matieres = Matiere.objects.all()
-    return render(request, 'liste_matiere.html',{'matieres': matieres})
+    return render(request, 'liste_matiere.html', {'matieres': matieres})
+
+
+@login_required(login_url="/utilisateur/connexion")
+@user_passes_test(verifSecretaire, login_url="/utilisateur/deconnexion")
+def modifier_matiere(request, id_matiere):
+    matiere = get_object_or_404(Matiere, pk=id_matiere)
+    form = ModifierMatiere(data={'titre': matiere.titre})
+    if request.method == 'POST':
+        form = ModifierMatiere(request.POST)
+        if form.is_valid():
+            try:
+                form.save(id_matiere)
+                messages.success(request, "Le titre de la matière a été modifié avec succès !")
+            except:
+                messages.error(request, "Erreur dans le formulaire !")
+        return HttpResponseRedirect(reverse('liste_matiere'))
+    else:
+        return render(request, 'modifier_matiere.html', {'form': form, 'matiere': matiere})
+
+
+def supprimer_matiere(request, id_matiere):
+    matiere = get_object_or_404(Matiere, pk=id_matiere)
+    try:
+        matiere.delete()
+        messages.success(request, "La matière a bien été supprimée !")
+    except:
+        messages.error(request, "Erreur dans la suppression de la matière!")
+    return HttpResponseRedirect(reverse('liste_matiere'))
