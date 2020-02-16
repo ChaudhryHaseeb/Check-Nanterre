3608c2b8145b8655c693e3a09232477a1fcbbe8a
Unknown <37009779@parisnanterre.fr>
2020-02-16 15:43:32 +0100

    API add update and reset password

--- utilisateur/api/urls.py
+++ utilisateur/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from utilisateur.api.views import user_connecte
+from utilisateur.api.views import user_connecte, user_modifier_mdp, user_reset_mdp
 
 app_name = 'utilisateur'
 
 urlpatterns = [
     path('connexion', obtain_auth_token, name="connexionApi"),
     path('user_connecte', user_connecte, name="userConnecteApi"),
+    path('modifier_mdp', user_modifier_mdp, name="userModifierMdpApi"),
+    path('reset_mdp', user_reset_mdp, name="userResetMdpApi"),
 ]
--- utilisateur/api/views.py
+++ utilisateur/api/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
@@ -19,3 +20,43 @@ def user_connecte(request):
     if request.method == "GET":
         serializer = UtilisateurSerializer(utilisateur)
         return Response(serializer.data)
+
+
+@api_view(['PUT',])
+def user_modifier_mdp(request):
+    try:
+        user = request.user
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "PUT":
+        data = {}
+        try:
+            user.set_password(request.data["password"])
+            user.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT',])
+def user_reset_mdp(request):
+    try:
+        user = User.objects.get(email=request.data["email"])
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "PUT":
+        data = {}
+        try:
+            password = User.objects.make_random_password()
+            user.set_password(password)
+            user.save()
+            # Envoi du nouveau mdp par mail
+            send_mail('Check_Nanterre : votre nouveau mot de passe',
+                      'Bonjour, voici votre nouveau mot de passe pour le site Check_Nanterre : ' + password,
+                      'check.nanterre@gmail.com',
+                      [request.data["email"]])
+            data["success"] = "update successful"
+            return Response(data=data)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
