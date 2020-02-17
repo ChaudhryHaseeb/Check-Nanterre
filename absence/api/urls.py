246ff95ecb0ea19cad00ac01178a772ff16e0e5e
Unknown <37009779@parisnanterre.fr>
2020-02-17 14:42:36 +0100

    Api justifier absence etudiant

--- absence/api/urls.py
+++ absence/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from absence.api.views import get_all_promotions, liste_etudiant_dans_promotion, absence_etudiant_connecte
+from absence.api.views import get_all_promotions, liste_etudiant_dans_promotion, absence_etudiant_connecte, \
+    absence_etudiant_justifier
 
 app_name = 'absence'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('promotions', get_all_promotions, name="allPromotionsApi"),
     path('promotions/etudiants/<int:id>', liste_etudiant_dans_promotion, name="listeEtudiantDansPromotionApi"),
     path('etudiant', absence_etudiant_connecte, name="absenceEtuConnecteApi"),
+    path('etudiant/justification/<int:id>', absence_etudiant_justifier, name="absenceEtuJustifApi"),
 ]
--- absence/api/views.py
+++ absence/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from absence.api.serializers import PromotionSerializer, AbsenceSeanceSerializer
-from absence.models import Promotion, PromotionEtudiants, AbsenceEtudiants, AbsenceSeance
+from absence.models import Promotion, PromotionEtudiants, AbsenceEtudiants, AbsenceSeance, Absence
 from utilisateur.api.serializers import UtilisateurSerializer
 from utilisateur.models import Utilisateur
 
@@ -58,4 +58,25 @@ def absence_etudiant_connecte(request):
             listeAbsSeance.append(AbsenceSeance.objects.get(absence_seance=obj.absence_etudiant))
         serializer = AbsenceSeanceSerializer(listeAbsSeance, many=True)
         return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+@permission_classes([IsAuthenticated])
+def absence_etudiant_justifier(request, id):
+    try:
+        abs = Absence.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        data = {}
+        try:
+            abs.justification = request.data["justification"]
+            abs.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 
